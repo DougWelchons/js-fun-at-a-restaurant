65df2d24a4eaef9d52d4eb515294a08deac871ab
Doug Welchons <doug.welchons@gmail.com>
2021-11-05 14:44:45 -0600

    Add code for passing meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+};
 
-}
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+};
 
+function addIngredients(ingredient, ingredients) {
+  if(ingredients.includes(ingredient)) {}
+  else {ingredients.push(ingredient)}
+  return ingredients
+};
+
+function formatPrice(price) {
+  return `$${price}`
+};
+
+function decreasePrice(price) {
+  return price * 0.90
+};
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
